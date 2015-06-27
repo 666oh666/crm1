f9b6345de25d076e8074954598e0cb963077addd
bullbear-github <bullbear@bull-bear.co.jp>
2015-06-27 11:07:47 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crm1::Application.config.secret_key_base = '87b9d239e10a489d950dfcc04dcbde470810999b9d08c9e205abed9be8a801267a0e6500ad29c9079e316f461f3a1e2ff7c3b72136e85bcfc21329d8f7d47390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crm1::Application.config.session_store :cookie_store, key: '_crm1_session'
